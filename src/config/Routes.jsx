2ef6f88cdd5b3d72ee42c8dea0516c412f67bc70
DinhVi2000 <vindde140058@fpt.edu.vn>
2022-07-06 20:42:09 +0700

    commit

--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -8,10 +8,7 @@ import Home from "../pages/Home";
 const Routes = () => {
   return (
     <Switch>
-      <Route path=":/category/search/:keyword" component={Catalog} />
-      <Route path=":/category/:id" component={Detail} />
-      <Route path=":/category" component={Catalog} />
-      <Route path="/" exact component={Home} />
+      <Route path="/" component={Home} />
     </Switch>
   );
 };
